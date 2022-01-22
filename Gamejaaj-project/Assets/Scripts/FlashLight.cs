fd1d857fd4b1541ac0e5b163a20499539d83f0f0
Kleuber <kleubercardoso@outlook.com>
2022-01-22 19:29:26 -0300

    FlashLight

---
+++ Gamejaaj-project/Assets/Scripts/FlashLight.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+   [SerializeField] private bool isOn = false;
+   [SerializeField] private GameObject lightSource;
+   [SerializeField] private AudioSource clickSound;
+   [SerializeField] private KeyCode lanternButton;
+   [SerializeField] private bool fail = false;
+
+    void Update()
+    {
+        TurnFlashLight();
+    }
+
+
+    public void TurnFlashLight()
+    {
+        if (Input.GetKeyDown(lanternButton))
+        {
+            if(!isOn && !fail)
+            {
+                fail = true;
+                lightSource.SetActive(true);
+                //clickSound.Play();
+                isOn = true;
+                StartCoroutine(Fail());
+            }
+            else
+            {
+                fail = true;
+                lightSource.SetActive(false);
+                //clickSound.Play();
+                isOn = false;
+                StartCoroutine(Fail());
+            }
+        }
+    }
+
+    IEnumerator Fail()
+    {
+        yield return new WaitForSeconds(0.25f);
+        fail = false;
+    }
+}
